ecd8f214a954e73dddc051d977679d218a27dbb2
Tuva Aarseth <tuva.emilie@hotmail.com>
2024-09-23 13:08:59 +0200

    Completed exercise

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,18 @@ const answer = []
 
 // Write your code below this line
 
+for (let i = 1; i <= 15; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        answer.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+        answer.push('Fizz')
+    } else if (i % 5 === 0) {
+        answer.push('Buzz')
+    } else {
+        answer.push(i)
+    }
+}
+
 
 
 
